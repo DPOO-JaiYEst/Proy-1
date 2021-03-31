6e2565fa104006d8f9eaedb084c2a3c479d4d7c0
Jaime Andres Torres B <69700780+XaurDesu@users.noreply.github.com>
2021-03-30 21:38:39 -0500

    añadida clase prerrequisito

--- src/modelo/prerequisito.java
+++ src/modelo/prerequisito.java
@@ -2,5 +2,27 @@ package modelo;
 
 public class prerequisito 
 {
-
+	
+	private String nombre;
+	
+	private String tipo;
+	
+	public String darNombre()
+	{
+		return nombre;
+	}
+	
+	public String tipo()
+	{
+		return tipo;
+	}
+	
+	//constructor
+	public prerequisito(String elNombre, String elTipo)
+	{
+		nombre = elNombre;
+		tipo = elTipo;
+		
+	}
+	
 }
